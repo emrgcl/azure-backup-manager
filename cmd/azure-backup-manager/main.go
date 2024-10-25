f52eb38c8985effa935aa6b7e6bebc4310930a56
Emre Güçlü <emreg@microsoft.com>
2024-10-25 16:36:32 +0300

    initial commit

--- cmd/azure-backup-manager/main.go
+++ cmd/azure-backup-manager/main.go
@@ -6,13 +6,13 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armsubscriptions"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storage/armstorage"
 	"github.com/charmbracelet/log"
 )
 
 func main() {
 	// Create a context for the authentication request
 	ctx := context.Background()
-
 	// Use DefaultAzureCredential to authenticate with Azure
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
@@ -25,17 +25,48 @@ func main() {
 		log.Fatalf("failed to create Azure subscriptions client: %v", err)
 	}
 
-	log.Info("Successfully created Azure subscriptions client")
+	subscriptions, err := GetSubscriptions(ctx, client)
+	if err != nil {
+		log.Fatalf("failed to get subscriptions: %v", err)
+	}
 
-	// Get the subscription details
+	fmt.Println("Stored Subscriptions:")
+	for _, sub := range subscriptions {
+		fmt.Printf("Subscription ID: %s, Subscription Name: %s\n", *sub.SubscriptionID, *sub.DisplayName)
+	}
+}
+
+// function to retrieve all storage accounts for the  specified subscription
+func GetStorageAccounts(ctx context.Context, client *armsubscriptions.Client, subscriptionID string) ([]*armstorage.Account, error) {
+	var storageAccounts []*armstorage.Account
+
+	pager := client.NewListStorageAccountsPager(subscriptionID, nil)
+	for pager.More() {
+		resp, err := pager.NextPage(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("failed to advance page: %v", err)
+		}
+		// Append each storage account object to the slice
+		storageAccounts = append(storageAccounts, resp.StorageAccountListResult.Value...)
+	}
+
+	return storageAccounts, nil
+}
+
+// GetSubscriptions retrieves all subscriptions for the authenticated user
+
+func GetSubscriptions(ctx context.Context, client *armsubscriptions.Client) ([]*armsubscriptions.Subscription, error) {
+	var subscriptions []*armsubscriptions.Subscription
+
 	pager := client.NewListPager(nil)
 	for pager.More() {
 		resp, err := pager.NextPage(ctx)
 		if err != nil {
-			log.Fatalf("failed to advance page: %v", err)
+			return nil, fmt.Errorf("failed to advance page: %v", err)
 		}
-		for _, sub := range resp.SubscriptionListResult.Value {
-			fmt.Printf("Subscription ID: %s, Subscription Name: %s\n", *sub.SubscriptionID, *sub.DisplayName)
-		}
+		// Append each subscription object to the slice
+		subscriptions = append(subscriptions, resp.SubscriptionListResult.Value...)
 	}
+
+	return subscriptions, nil
 }
